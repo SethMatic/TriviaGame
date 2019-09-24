e9f248a20f9db4c25f06dd2af1f315329c234725
Seth Dousman <sdousman3@gmail.com>
2019-09-24 12:58:14 -0400

    added timer and next question function

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -7,9 +7,51 @@ let lost = 0;
 let timer;
 
 
+///next question when timer is done
+
+function nextQuestion(){
+
+    const isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
+    if(isQuestionOver){
+        console.log("Game over homie!");
+    } else {
+       currentQuestion++;
+        loadQuestion();
+    }
+
+
+
+}
+
+////get timer to work
+
+function timeUp() {
+    clearInterval(timer);
+
+    lost++;
+
+    nextQuestion();
+}
+
+function countDown(){
+    counter--;
+    $('#time').html('Timer: ' + counter);
+
+    if (counter === 0){
+        timeUp();
+    }
+}
+
+
+
 ///show questions in browser
 
 function loadQuestion() {
+
+counter = 30;
+timer = setInterval(countDown,1000);
+
+
     const question = quizQuestions[currentQuestion].question;
     const choices = quizQuestions[currentQuestion].choices;
 
