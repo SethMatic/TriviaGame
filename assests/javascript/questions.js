40a9f0316fb3bbbbdc48025d8a0df689b995d65c
Seth Dousman <sdousman3@gmail.com>
2019-09-24 12:33:53 -0400

    added questions and answers.

---
+++ assests/javascript/questions.js
@@ -0,0 +1,25 @@
+const quizQuestions = [
+    {
+        question: 'What flavor is the green colored Haribo Gummy Bear',
+        choices: ["Orange","Green Apple","Strawberry","Pineapple"],
+        coorrectAnswer: "Strawberry"
+    },
+
+    {
+        question: 'How did the breathmint "Life Savers" get its name?',
+        choices: ["Fights Cavaties","Freshens Breath","Prevents Choking","Whitens Teeth"],
+        coorrectAnswer:"Prevents Choking"
+    },
+
+
+    {
+        question: 'How many different colors of M&Ms are in the regular bag?',
+        choices: ["4","3","6","5"],
+        coorrectAnswer:"6"
+    }
+
+
+
+
+
+];
